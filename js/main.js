478b70355d18da4bbca852e65a964d5eba1d0efc
michael yuen <michaelcyyuen@gmail.com>
2020-09-21 20:57:49 +0800

    remove unused js

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ new WOW().init();
 
 //Notify me 
 
-$("#notifyMe").notifyMe();
+// $("#notifyMe").notifyMe();
 
 
 // screenshots carousel
@@ -82,42 +82,42 @@ $(document).ready(function() {
 
     // magnific popup
 
-    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-        disableOn: 700,
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-
-        fixedContentPos: false
-    });
-
-    $('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-            titleSrc: function(item) {
-                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-            }
-        }
-    });
+    // $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    //     disableOn: 700,
+    //     type: 'iframe',
+    //     mainClass: 'mfp-fade',
+    //     removalDelay: 160,
+    //     preloader: false,
+    //
+    //     fixedContentPos: false
+    // });
+    //
+    // $('.popup-gallery').magnificPopup({
+    //     delegate: 'a',
+    //     type: 'image',
+    //     tLoading: 'Loading image #%curr%...',
+    //     mainClass: 'mfp-img-mobile',
+    //     gallery: {
+    //         enabled: true,
+    //         navigateByImgClick: true,
+    //         preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+    //     },
+    //     image: {
+    //         tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    //         titleSrc: function(item) {
+    //             return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+    //         }
+    //     }
+    // });
 
     //Company Stats Count
 
-    $('#stat').waypoint(function() {
-        //Execute CountTo Plugin
-        $('.stats-count').countTo();
-    }, {
-        offset: 300
-    });
+    // $('#stat').waypoint(function() {
+    //     //Execute CountTo Plugin
+    //     $('.stats-count').countTo();
+    // }, {
+    //     offset: 300
+    // });
 
 });
 
